06316bbac924a03bae96384ca05fa158610352cd
Kumar-Aditya-Pratap <128625213+Kumar-Aditya-Pratap@users.noreply.github.com>
2026-02-06 14:31:50 +0530

    [Aditya Pratap]Added :UC5: console-based user interaction

---
+++ src/UserInterface.java
@@ -0,0 +1,44 @@
+import java.util.List;
+import java.util.Scanner;
+
+public class UserInterface {
+
+    public static void main(String[] args) {
+
+        VesselUtil vesselUtil = new VesselUtil();
+        Scanner scanner = new Scanner(System.in);
+
+        // initialize predefined vessels
+        vesselUtil.getVesselList();
+
+        System.out.println("Enter the Vessel Id to check speed");
+        String vesselId = scanner.nextLine();
+
+        Vessel vessel = vesselUtil.getVesselById(vesselId);
+
+        if (vessel != null) {
+            System.out.println(
+                    vessel.getVesselId() + " | " +
+                            vessel.getVesselName() + " | " +
+                            vessel.getVesselType() + " | " +
+                            vessel.getAverageSpeed() + " knots"
+            );
+        } else {
+            System.out.println("Vessel Id " + vesselId + " not found");
+        }
+
+        System.out.println("High performance vessels are");
+        List<Vessel> highPerformanceVessels = vesselUtil.getHighPerformanceVessels();
+
+        for (Vessel v : highPerformanceVessels) {
+            System.out.println(
+                    v.getVesselId() + " | " +
+                            v.getVesselName() + " | " +
+                            v.getVesselType() + " | " +
+                            v.getAverageSpeed() + " knots"
+            );
+        }
+
+        scanner.close();
+    }
+}
