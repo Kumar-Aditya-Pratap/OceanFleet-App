41a7885ae3cc6f4661b917541b5102211c07c0d4
Kumar-Aditya-Pratap <128625213+Kumar-Aditya-Pratap@users.noreply.github.com>
2026-02-06 14:17:09 +0530

    [Aditya Pratap]Added : UC4/method to retrieve high performance vessels

--- src/Vessel.java
+++ src/Vessel.java
@@ -3,7 +3,7 @@
 
 /*
 * UC1-Creating Vessel Model
-* version 3.0
+* version 4.0
 * Author:Kumar Aditya Pratap
 */
 
--- src/VesselUtil.java
+++ src/VesselUtil.java
@@ -19,4 +19,23 @@ public class VesselUtil {
         }
         return null;
     }
+    public List<Vessel> getHighPerformanceVessels() {
+
+        List<Vessel> result = new ArrayList<>();
+        double maxSpeed = 0;
+
+        for (Vessel vessel : vesselList) {
+            if (vessel.getAverageSpeed() > maxSpeed) {
+                maxSpeed = vessel.getAverageSpeed();
+            }
+        }
+
+        for (Vessel vessel : vesselList) {
+            if (vessel.getAverageSpeed() == maxSpeed) {
+                result.add(vessel);
+            }
+        }
+
+        return result;
+    }
 }
